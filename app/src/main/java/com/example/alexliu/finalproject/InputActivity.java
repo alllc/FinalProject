7a1cacdb063a8a34ad26a1f38f1ed5064b67d6a9
alllc <llx789456@hotmail.com>
2016-07-01 23:19:51 -0700

    d

--- app/src/main/java/com/example/alexliu/finalproject/InputActivity.java
+++ app/src/main/java/com/example/alexliu/finalproject/InputActivity.java
@@ -1,7 +1,44 @@
 package com.example.alexliu.finalproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by alexliu on 16-07-01.
  */
-public class InputActivity {
+public class InputActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+    }
+
+    //button click to open income page
+    public void incomePage(View view){
+        Intent intent = new Intent(this, IncomeActivity.class);
+        startActivity(intent);
+    }
+    //button click to open expense page
+    public void expensePage(View view){
+        Intent intent = new Intent(this, ExpenseActivity.class);
+        startActivity(intent);
+    }
+    //button click to open input page
+    public void inputPage(View view){
+        Intent intent = new Intent(this, ExpenseActivity.class);
+        startActivity(intent);
+    }
+    //button click to open result page
+    public void resultPage(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    //button click to open setting page
+    public void settingPage(View view){
+        Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/alexliu/finalproject/MainActivity.java
+++ app/src/main/java/com/example/alexliu/finalproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,11 +23,35 @@ public class MainActivity extends AppCompatActivity {
         String loginPassword = sharedPrefs.getString("loginPassword", DEFAULT);
         //check if there is a data
         Toast.makeText(this,"loading to log in page",Toast.LENGTH_LONG).show();
-        if(!loginUser.equals(DEFAULT) && !loginPassword.equals(DEFAULT)){
-            Toast.makeText(this,"No user log in, loading to log in page",Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
+//        if(!loginUser.equals(DEFAULT) && !loginPassword.equals(DEFAULT)){
+//            Toast.makeText(this,"No user log in, loading to log in page",Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            startActivity(intent);
+//        }
     }
-
+    //button click to open income page
+    public void incomePage(View view){
+        Intent intent = new Intent(this, IncomeActivity.class);
+        startActivity(intent);
+    }
+    //button click to open expense page
+    public void expensePage(View view){
+        Intent intent = new Intent(this, ExpenseActivity.class);
+        startActivity(intent);
+    }
+    //button click to open input page
+    public void inputPage(View view){
+        Intent intent = new Intent(this, ExpenseActivity.class);
+        startActivity(intent);
+    }
+    //button click to open result page
+    public void resultPage(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    //button click to open setting page
+    public void settingPage(View view){
+        Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
+    }
 }
