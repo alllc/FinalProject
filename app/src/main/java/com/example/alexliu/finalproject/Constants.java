cb13a81feb9f34208f54262e696a5e5d6df39bfe
alllc <llx789456@hotmail.com>
2016-07-03 15:04:49 -0700

    d

--- app/src/main/java/com/example/alexliu/finalproject/Constants.java
+++ app/src/main/java/com/example/alexliu/finalproject/Constants.java
@@ -4,4 +4,13 @@ package com.example.alexliu.finalproject;
  * Created by alexliu on 2016-07-03.
  */
 public class Constants {
+    public static final String DATABASE_NAME = "moneydatabase";
+    public static final String TABLE_NAME = "USAGETABLE";
+    public static final String UID = "_id";
+    public static final String INPUT = "Input";
+    public static final String NAME = "Name";
+    public static final String TYPE = "Type";
+    public static final String AMOUNT = "Amount";
+    public static final String DATE = "Date";
+    public static final int DATABASE_VERSION = 1;
 }
