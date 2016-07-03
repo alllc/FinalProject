fd4e3a6d56ad67482a91bda25433d6d8ae35ae52
alllc <llx789456@hotmail.com>
2016-07-03 16:11:52 -0700

    16:11 7.3

--- app/src/main/java/com/example/alexliu/finalproject/InputActivity.java
+++ app/src/main/java/com/example/alexliu/finalproject/InputActivity.java
@@ -4,15 +4,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by alexliu on 16-07-01.
  */
 public class InputActivity extends AppCompatActivity {
+    EditText  moneyName,  moneyAmt, moneyDate, selectType;
+    Spinner moneyInput,moneyType;
+    MyDataBase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        moneyInput = (Spinner)findViewById(R.id.inputSpinner);
+        moneyName = (EditText)findViewById(R.id.nameEditTxt);
+        moneyType = (Spinner)findViewById(R.id.typeSpinner);
+        moneyAmt = (EditText)findViewById(R.id.amountEditTxt);
+        moneyDate = (EditText)findViewById(R.id.dateEditTxt);
+
+        db = new MyDataBase(this);
+
+    }
+    public void submitButton (View view)
+    {
+      //  String input = moneyInput.
+        String name = moneyName.getText().toString();
+      //  String type = moneyType.getText().toString();
+        String amount = moneyAmt.getText().toString();
+        String date = moneyDate.getText().toString();
+        Toast.makeText(this, name + type + amount + date, Toast.LENGTH_SHORT).show();
+        long id = db.insertData(name, type, amount, date);
+        if (id < 0)
+        {
+            Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
 
     }
 
--- app/src/main/java/com/example/alexliu/finalproject/MyDataBase.java
+++ app/src/main/java/com/example/alexliu/finalproject/MyDataBase.java
@@ -1,7 +1,70 @@
 package com.example.alexliu.finalproject;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by alexliu on 2016-07-03.
  */
 public class MyDataBase {
+    private SQLiteDatabase db;
+    private Context context;
+    private final MyHelper helper;
+
+    public MyDataBase (Context c){
+        context = c;
+        helper = new MyHelper(context);
+    }
+
+    public long insertData (String input, String name, String type, String amount, String date)
+    {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Constants.INPUT, input);
+        contentValues.put(Constants.NAME, name);
+        contentValues.put(Constants.TYPE, type);
+        contentValues.put(Constants.AMOUNT, amount);
+        contentValues.put(Constants.DATE, date);
+        long id = db.insert(Constants.TABLE_NAME, null, contentValues);
+        return id;
+    }
+
+    public Cursor getData()
+    {
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        String[] columns = {Constants.UID, Constants.INPUT, Constants.NAME, Constants.TYPE, Constants.AMOUNT, Constants.DATE};
+        Cursor cursor = db.query(Constants.TABLE_NAME, columns, null, null, null, null, null);
+        return cursor;
+    }
+
+
+    public Cursor getSelectedData(String type)
+    {
+        //select plants from database of type 'herb'
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String[] columns = {Constants.UID, Constants.INPUT,Constants.NAME, Constants.TYPE, Constants.AMOUNT, Constants.DATE};
+
+        String selection = Constants.TYPE + "='" +type+ "'";  //Constants.TYPE = 'type'
+        Cursor cursor = db.query(Constants.TABLE_NAME, columns, selection, null, null, null, null);
+        return cursor;
+//        StringBuffer buffer = new StringBuffer();
+//        while (cursor.moveToNext()) {
+//
+//            int index1 = cursor.getColumnIndex(Constants.NAME);
+//            int index2 = cursor.getColumnIndex(Constants.TYPE);
+//            int index3 = cursor.getColumnIndex(Constants.LOCATION);
+//            int index4 = cursor.getColumnIndex(Constants.LATIN);
+//            String plantName = cursor.getString(index1);
+//            String plantType = cursor.getString(index2);
+//            String plantLoc = cursor.getString(index3);
+//            String plantLat = cursor.getString(index4);
+//            buffer.append(plantName + " " + plantType + plantLoc + "" + plantLat +"\n");
+//        }
+//        return buffer.toString();
+    }
+
 }
+
