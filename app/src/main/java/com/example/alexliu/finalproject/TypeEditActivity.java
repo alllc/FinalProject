6bd6377308fa5e850b1519cb4a86e30a32d7eddc
alllc <llx789456@hotmail.com>
2016-08-01 18:36:18 -0700

    k

--- app/src/main/java/com/example/alexliu/finalproject/TypeEditActivity.java
+++ app/src/main/java/com/example/alexliu/finalproject/TypeEditActivity.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TabHost;
 import android.widget.Toast;
+
 
 /**
  * Created by alexliu on 16-07-27.
@@ -19,11 +23,41 @@ public class TypeEditActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_typeedit);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar_setting);
 
+        TabHost thost = (TabHost) findViewById(R.id.tabhost);
+        thost.setup();
+
+        LayoutInflater.from(this).inflate(R.layout.activity_incomeedit, thost.getTabContentView());
+        LayoutInflater.from(this).inflate(R.layout.activity_expenseedit, thost.getTabContentView());
+
+        thost.addTab(thost.newTabSpec("tab1").setIndicator("Income").setContent(R.id.tab01));
+        thost.addTab(thost.newTabSpec("tab2").setIndicator("Expense").setContent(R.id.tab02));
+
+        thost.setOnTabChangedListener(new TabHost.OnTabChangeListener(){
+            @Override
+            public void onTabChanged(String tabId) {
+                if (tabId.equals("tab1")) {   //第一个标签
+                    Toast.makeText(TypeEditActivity.this, "Income", Toast.LENGTH_SHORT).show();
+                }
+                if (tabId.equals("tab2")) {   //第二个标签
+                    Toast.makeText(TypeEditActivity.this, "Expense", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void LogOut (View view){
+        SharedPreferences sharedPrefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString("loginUser", DEFAULT);
+        editor.putString("loginPassword", DEFAULT);
+        editor.commit();
+        Toast.makeText(this,"user log out, loading to log in page",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 
